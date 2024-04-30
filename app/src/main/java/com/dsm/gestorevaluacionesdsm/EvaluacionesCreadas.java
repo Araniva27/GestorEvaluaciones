8c0613afc2ee26a8d2bd27661be62556941fec4e
Araniva27 <manuelaraniva07@gmail.com>
2024-04-29 19:29:55 -0600

    Creacion de vista principal admin

---
+++ app/src/main/java/com/dsm/gestorevaluacionesdsm/EvaluacionesCreadas.java
@@ -0,0 +1,14 @@
+package com.dsm.gestorevaluacionesdsm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EvaluacionesCreadas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evaluaciones_creadas);
+    }
+}
